77a8aa8be715f923d89b1889d1f8c9c8db7c342a
Nic <niclin0226@gmail.com>
2019-08-29 14:45:47 +0800

    Basic use

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -3,7 +3,37 @@
     <h1>租屋虧多少</h1>
 
     <div>成本計算開始:</div>
-    <input type="text" v-model="price" placeholder="開價多少錢">
+
+    <input type="text" v-model="cost" placeholder="15000">
+
+    <ul>
+      <li>
+        <input type="checkbox" v-model="additionGroup" value="washingMachine" />
+        <label>沒有洗衣機</label>
+      </li>
+
+      <li>
+        <input type="checkbox" v-model="additionGroup" value="bed" />
+        <label>沒有床墊</label>
+      </li>
+
+      <li>
+        <input type="checkbox" v-model="additionGroup" value="network" />
+        <label>沒有網路</label>
+      </li>
+
+      <li>
+        <input type="checkbox" v-model="additionGroup" value="dispenser" />
+        <label>沒有飲水機</label>
+      </li>
+
+      <li>
+        <input type="checkbox" v-model="additionGroup" value="garbageCollection" />
+        <label>沒有代收垃圾</label>
+      </li>
+    </ul>
+
+    <div>總計裸屋成本約：{{ calculateTotalCost }}</div>
 
   </div>
 </template>
@@ -12,13 +42,29 @@
 export default {
   name: 'app',
   data: () => ({
-    message: "HELLO"
+    cost: 0,
+    additionGroup: [],
+    costLists: {
+      "washingMachine": 1000,
+      "bed": 1000,
+      "network": 600,
+      "garbageCollection": 500,
+      "dispenser": 300
+    }
   }),
-  computed: { },
-  created () { },
-  mounted () { },
-  methods: {
+  computed: {
+    calculateTotalCost: function() {
+      let additionCost = 0
+      const { additionGroup, costLists } = this
 
+      Object.keys(costLists).forEach(function(key, index) {
+        if (additionGroup.includes(key)) {
+         additionCost += costLists[key]
+        }
+      })
+
+      return this.cost - additionCost
+    }
   }
 }
 </script>
--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import AppComponent from "./App/App.vue";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.component("app-component", AppComponent);
 
