fddaa04e3afc4d2ceb4aac4623891cfa65e885a9
Nic <niclin0226@gmail.com>
2019-09-03 13:21:24 +0800

    Fetch 591 price

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main_app">
     <h1>租屋虧多少</h1>
+    <p>當前網站 {{currentUrl}}</p>
+    <p>當前租金 {{rentPrice}}</p>
 
     <div>成本計算開始:</div>
 
@@ -33,10 +35,13 @@
       </li>
     </ul>
 
-    <div>總計裸屋成本約：{{ calculateTotalCost }}</div>
+
+    <div>總計裸屋成本約：{{ calculateTotalCost }} </div>
 
   </div>
 </template>
+
+
 
 <script>
 export default {
@@ -50,8 +55,26 @@ export default {
       "network": 600,
       "garbageCollection": 500,
       "dispenser": 300
-    }
+    },
+    currentUrl: null,
+    rentPrice: 0
   }),
+  mounted() {
+    const setCurrentUrl = (url) => { this.currentUrl = url }
+    const setRentPrice = (price) => { this.rentPrice = price }
+
+    chrome.tabs.executeScript({
+        code: '(' + function(params) {
+          return { price: document.getElementsByClassName("price")[0].innerText };
+        } + ')(' + JSON.stringify() + ');'
+    }, function(results) {
+      setRentPrice(results[0].price)
+    });
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      setCurrentUrl(tabs[0].url)
+    });
+  },
   computed: {
     calculateTotalCost: function() {
       let additionCost = 0
@@ -64,6 +87,11 @@ export default {
       })
 
       return this.cost - additionCost
+    }
+  },
+  methods: {
+    setCurrentUrl(url) {
+      this.currentUrl = url
     }
   }
 }
